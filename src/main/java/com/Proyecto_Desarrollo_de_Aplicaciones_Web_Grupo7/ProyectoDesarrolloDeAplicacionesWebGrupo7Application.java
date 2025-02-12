e7bdd24d7288052ddb16c290dd657f4776bda347
JDVargasR <josedavargasr@gmail.com>
2025-02-12 15:20:18 -0600

    Generación Inicial del Proyecto y del Repositorio

---
+++ src/main/java/com/Proyecto_Desarrollo_de_Aplicaciones_Web_Grupo7/ProyectoDesarrolloDeAplicacionesWebGrupo7Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Desarrollo_de_Aplicaciones_Web_Grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesarrolloDeAplicacionesWebGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDesarrolloDeAplicacionesWebGrupo7Application.class, args);
+	}
+
+}
