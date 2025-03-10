feb52b1dfbaf5b56ef9bb924d59a0226ce7294db
Josue Navarro <josue13398@icloud.com>
2025-03-10 16:25:29 -0600

    u

---
+++ src/main/java/com/Proyecto_Grupo7/Proyecto_Grupo7Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto_Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto_Grupo7Application.class, args);
+	}
+
+}
