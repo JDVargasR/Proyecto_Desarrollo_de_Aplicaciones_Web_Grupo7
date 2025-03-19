7d99961633f123acc6b34e1e1e61f2766ce37c4e
Abraham3105 <acascantebadilla31@gmail.com>
2025-03-19 12:27:13 -0600

    Arreglo de ventana blog

--- src/main/java/com/Proyecto_Grupo7/service/impl/BlogServiceImpl.java
+++ src/main/java/com/Proyecto_Grupo7/service/impl/BlogServiceImpl.java
@@ -11,16 +11,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author gmora
  */
 @Service
-public class BlogServiceImpl implements BlogService{
-    
+public class BlogServiceImpl implements BlogService {
+
     @Autowired
     private BlogDao blogDao;
-    
+
     @Override
     @Transactional(readOnly = true)
     public List<Blog> getBlog(boolean activos) {
@@ -30,7 +31,7 @@ public class BlogServiceImpl implements BlogService{
         }
         return lista;
     }
-    
+
     @Override
     @Transactional(readOnly = true)
     public Blog getBlog(Blog blog) {
